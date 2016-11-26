44ecc62140d05e7765ec0adea31b1516d08fc6be
Roskis <antti.karkinen@aalto.fi>
2016-11-26 13:21:06 +0200

    lane group

--- js/Game.js
+++ js/Game.js
@@ -11,36 +11,45 @@ Quantumgator.Game.prototype = {
     this.background.anchor.setTo(0.5, 0.5);
     //game.T is the current temperature
     this.game.T = 0;
-    this.game.temperatureText = this.game.add.text(100,20, "temperature", {font:"12px Arial", fill:"#000000"});
+    this.game.temperatureText = this.game.add.text(100, 20, "temperature", {font:"20px Arial", fill:"#000000"});
     this.game.temperatureText.anchor.set(0.5);
-    
+
+    this.game.lanes = this.game.add.group();
+    for (i = 0; i < 5; i++) {
+      this.game.lanes.create(0, 80+100*i, 'lane');
+    }
+
+    this.game.lanes.forEach(function(item) {
+      item.scale.setTo(window.innerWidth/item.width, 1);
+    });
+
   },
   update: function() {
     this.game.temperatureText.text = "temperature: " + this.game.T;
   },
   //detect player collision
   playerHit: function(player, blocklayer) {
-    
+
   },
   //detection for hitting collectables
   collect: function(player, collectable) {
-    
+
   },
   //generated collectables to the game view
   createCollectables: function() {
-    
+
   },
   //declare game over
   gameOver: function(){
-    
+
   },
   //moves player up, down and allows to enter QUANTUM MODE!!!!!!!!!
   playerControl: function(){
-    
+
   },
   //changes the temperature in range [0,20]
   changeTemperature: function(num){
   this.game.T += num;
   }
-  
+
 }
