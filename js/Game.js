1d7bf589ec8fa571c7f2fa4351ed30c902c73030
Christian Westerlund <ca.westerlund@gmail.com>
2016-11-26 10:48:06 +0000

    temperature debuggit add .!

--- js/Game.js
+++ js/Game.js
@@ -9,10 +9,14 @@ Quantumgator.Game.prototype = {
   create: function() {
     this.background = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'background');
     this.background.anchor.setTo(0.5, 0.5);
+    //game.T is the current temperature
+    this.game.T = 0;
+    this.game.temperatureText = this.game.add.text(100,20, "temperature", {font:"12px Arial", fill:"#000000"});
+    this.game.temperatureText.anchor.set(0.5);
     
   },
   update: function() {
-
+    this.game.temperatureText.text = "temperature: " + this.game.T;
   },
   //detect player collision
   playerHit: function(player, blocklayer) {
@@ -33,6 +37,10 @@ Quantumgator.Game.prototype = {
   //moves player up, down and allows to enter QUANTUM MODE!!!!!!!!!
   playerControl: function(){
     
+  },
+  //changes the temperature in range [0,20]
+  changeTemperature: function(num){
+  this.game.T += num;
   }
   
 }
