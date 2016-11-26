1bb33c47bcd417021a4b8630d519990903f02cf5
Christian Westerlund <ca.westerlund@gmail.com>
2016-11-26 10:24:50 +0000

    skeleton for game.js

--- js/Game.js
+++ js/Game.js
@@ -9,8 +9,30 @@ Quantumgator.Game.prototype = {
   create: function() {
     this.background = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'background');
     this.background.anchor.setTo(0.5, 0.5);
+    
   },
   update: function() {
 
+  },
+  //detect player collision
+  playerHit: function(player, blocklayer) {
+    
+  },
+  //detection for hitting collectables
+  collect: function(player, collectable) {
+    
+  },
+  //generated collectables to the game view
+  createCollectables: function() {
+    
+  },
+  //declare game over
+  gameOver: function(){
+    
+  },
+  //moves player up, down and allows to enter QUANTUM MODE!!!!!!!!!
+  playerControl: function(){
+    
   }
+  
 }
