7f15ede370f7f374ba4e6fccbb1838dab6917082
aurochs-angels <aino.nummikallio@gmail.com>
2016-11-27 13:16:28 +0200

    gameover js added to html and main.js

--- js/main.js
+++ js/main.js
@@ -5,5 +5,6 @@ Quantumgator.game = new Phaser.Game(800, 600, Phaser.AUTO, '');
 Quantumgator.game.state.add('Boot', Quantumgator.Boot);
 Quantumgator.game.state.add('Preload', Quantumgator.Preload);
 Quantumgator.game.state.add('Game', Quantumgator.Game);
+Quantumgator.game.state.add('gameover', Quantumgator.gameover);
 
 Quantumgator.game.state.start('Boot');
